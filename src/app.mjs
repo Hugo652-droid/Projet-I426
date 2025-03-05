e9e90a4e5bae1d1ebc94700dc2c12ad6b24b4ae4
pw10thu <pw10thu@eduvaud.ch>
2025-03-05 16:38:31 +0100

    Feat: Fait les requettes http

--- src/app.mjs
+++ src/app.mjs
@@ -1,10 +1,26 @@
 import express from "express";
+import {productsRouter} from "./routes/produits.mjs";
+import {initDb, sequelize} from "./db/squelize.mjs";
 
 const app = express();
 const port = 3000;
+
+sequelize
+    .authenticate()
+    .then((_) => console.log("La connexion à la base de données a bien été établie"))
+    .catch((error) => console.error(`Impossible de se connecter à la DB: ${error}`));
+
+
+initDb();
+
 
 app.use(express.json());
 
 app.listen(port, () =>
     console.log(`Notre application est démarée sur : http://localhost:${port}`)
-  );
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.use("/produits", productsRouter);
