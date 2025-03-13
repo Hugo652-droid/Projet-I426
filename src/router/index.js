99435121ba80b4e4d3194c32fa8eb38267e11e80
Marco Mascellino <pp47nln@eduvaud.ch>
2025-03-13 15:11:56 +0100

    feat: detailo magasin

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ListMagasin from "@/views/ListMagasin.vue";
+import DetailMagasin from "@/views/DetailMagasin.vue";
 
 const routes = [
   {
@@ -12,6 +13,12 @@ const routes = [
     path: '/ListMagasin',
     name: 'ListMagasin',
     component: ListMagasin,
+  },
+  {
+    path: "/DetailMagasin",
+    name: "DetailMagasin",
+    component: DetailMagasin,
+
   }
 ]
 
