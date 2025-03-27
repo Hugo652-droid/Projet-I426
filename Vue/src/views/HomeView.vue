7f81cdaf3b85fbf7cd85deb5553109242fbe2b88
Sky-tales <deyan.tucev2007@gmail.com>
2025-03-27 14:43:10 +0100

    merge

---
+++ Vue/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <NavBar></NavBar>
+
+
+
+</template>
+
+<script>
+// @ is an alias to /src
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+  name: 'HomeView',
+  components: {
+    NavBar,}
+}
+</script>
